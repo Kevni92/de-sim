da5226bcddc84260277bb3530c050828ddd92baf
Kevni92 <mail@kevinkrone.de>
2026-07-13 16:18:56 +0200

    feat: include population tax provenance styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./evidence-tooltips.css";
 import "./milestone2.css";
 import "./transparency.css";
 import "./income-tax.css";
+import "./income-tax-population.css";
 import "./revenue-modules.css";
 import "./expense-modules.css";
 import "./population.css";
