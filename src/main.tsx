c632218f9d36ac8dd1b75ac96b94ce59d74e2e40
Kevni92 <mail@kevinkrone.de>
2026-07-13 13:04:12 +0200

    style: Transparenzoberfläche laden

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import "./milestone2.css";
+import "./transparency.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
